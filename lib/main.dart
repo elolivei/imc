435ed37c4f78da5ddac2b76195278aeef86ee245
Elias Carneiro de Oliveira <elolivei@gmail.com>
2024-09-08 11:38:47 -0300

    Versão Responsiva e statefull

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Aplicação Responsiva'),
+        ),
+        body: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage();
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> rotuloBotao = [
+    "7",
+    "8",
+    "9",
+    "Enter",
+    "4",
+    "5",
+    "6",
+    ".",
+    "1",
+    "2",
+    "3",
+    "0"
+  ];
+  final List<double> valorBotao = [
+    7.0,
+    8.0,
+    9.0,
+    -1,
+    4.0,
+    5.0,
+    6.0,
+    -2,
+    1.0,
+    2.0,
+    3.0,
+    0.0
+  ];
+  final List<String> rotuloLabel = ["Peso", "Altura", "IMC"];
+  final List<String> estado = [
+    'assets/images/magro.png',
+    'assets/images/normal.png',
+    'assets/images/gordo.png'
+  ];
+  int _estadoIndex = 1;
+  List<double> valor = [0.1, 0.2, 0.3];
+
+  void botaoAcionado(int index) {
+    setState(() {
+      valor[0] = valorBotao[index];
+      _estadoIndex++;
+      _estadoIndex = _estadoIndex > 2 ? 0 : _estadoIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                estado[_estadoIndex],
+                width: constraints.maxWidth * 0.2,
+                height: constraints.maxHeight * 0.3,
+                //fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: List.generate(3, (index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(rotuloLabel[index]),
+                          ),
+                          Expanded(
+                            child: Text(
+                              (valor[index]).toString(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                  ),
+                  itemCount: 12,
+                  itemBuilder: (context, index) {
+                    return ElevatedButton(
+                      onPressed: () {
+                        botaoAcionado(index);
+                      },
+                      child: Text(rotuloBotao[index]),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
